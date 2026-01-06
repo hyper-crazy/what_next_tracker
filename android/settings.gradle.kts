b8e06f4da5235369bdac71d39e50f66a08bc5f8e
hyper-crazy <morshedulislammaurf00@gmail.com>
2026-01-06 14:52:22 +0600

    Fix Gradle repositories to resolve 403 error

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,13 +1,5 @@
 pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
-        }
-
+    def flutterSdkPath = settings.extraProperties.get("flutter.sdk") as String
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
@@ -19,8 +11,17 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.android.application") version "8.1.0" apply false
+    // We force a stable Kotlin version here to avoid the 2.0.21 fetch error
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 include(":app")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
